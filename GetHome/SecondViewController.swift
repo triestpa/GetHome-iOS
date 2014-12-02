2907792a1f94f3014d677cdab9fe629c2548ac12
triestpa <triestpa@grinnell.edu>
2014-12-02 19:54:27 +0100

    Initial Commit

---
+++ GetHome/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GetHome
+//
+//  Created by Patrick on 12/2/14.
+//  Copyright (c) 2014 Patrick Triest. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
