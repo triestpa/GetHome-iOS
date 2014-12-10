5dc10132d4f86ea21d7623d67416921cdae3e41d
triestpa <triestpa@grinnell.edu>
2014-12-10 17:46:35 +0100

    Strip down project to just the map view

--- GetHome/GetHome-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  RESideMenu-Bridging-Header.h
-//  GetHome
-//
-//  Created by Patrick on 12/3/14.
-//  Copyright (c) 2014 Patrick Triest. All rights reserved.
-//
-
-#import "RESideMenu.h"
--- Podfile
+++ Podfile
@@ -1,3 +1,1 @@
 platform :ios, '8.0'
-
-pod 'RESideMenu', '~> 4.0.7'
