dec3afe76a978f4b73e7ed6305cca522e437288e
triestpa <triestpa@grinnell.edu>
2014-12-10 18:03:22 +0100

    Add intro and progress view pods to cocoa

---
+++ GetHome/GetHome-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  GetHome-Bridging-Header.h
+//  GetHome
+//
+//  Created by Patrick on 12/10/14.
+//  Copyright (c) 2014 Patrick Triest. All rights reserved.
+//
+
+
+#import "MRProgress.h"
+#import "EAIntroView.h"
+
--- Podfile
+++ Podfile
@@ -1,1 +1,4 @@
 platform :ios, '8.0'
+
+pod 'MRProgress', '~> 0.4.3'
+pod 'EAIntroView', '~> 2.5.0'
